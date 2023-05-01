5f8ac204b35a6b71f4048fb884cf76dbf65e3f6d
Sarah Wilkes <aremnius@gmail.com>
2023-05-01 17:21:08 -0400

    play button change on hover

--- src/components/PlayWord/PlayWord.js
+++ src/components/PlayWord/PlayWord.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./PlayWord.module.css";
-import playIcon from "../../assets/images/icon-play.svg";
+// import playIcon from "../../assets/images/icon-play.svg";
 
 function PlayWord({ wordInfo }) {
   // URL or null
@@ -16,13 +16,22 @@ function PlayWord({ wordInfo }) {
   return (
     <>
       <audio src={audioFile} ref={audioRef} />
-      <input
-        className={styles.button}
-        type="image"
-        src={playIcon}
-        alt="Play pronuncation (if available)"
-        onClick={() => audioRef.current.play()}
-      />
+      <button className={styles.button} onClick={() => audioRef.current.play()}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="75"
+          height="75"
+          viewBox="0 0 75 75"
+          role="img"
+          aria-labelledby="title"
+        >
+          <title id="title">Play pronuncation</title>
+          <g className={styles.svg} fill="#A445ED" fillRule="evenodd">
+            <circle className={styles.circle} cx="37.5" cy="37.5" r="37.5" opacity=".25" />
+            <path d="M29 27v21l21-10.5z" />
+          </g>
+        </svg>
+      </button>
     </>
   );
 }
