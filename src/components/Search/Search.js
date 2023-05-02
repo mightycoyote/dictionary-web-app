69f63d242cef39596a816aaf34b68d2d0d9b547f
Sarah Wilkes <aremnius@gmail.com>
2023-05-02 15:28:41 -0400

    work on CSS

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,31 +1,56 @@
-import React from 'react';
-import { CForm, CFormLabel, CFormInput, CFormText } from '@coreui/react';
-import styles from './Search.module.css';
+import React from "react";
+import { CForm, CFormLabel, CFormInput, CFormText } from "@coreui/react";
+import styles from "./Search.module.css";
 
 function Search({ handleSubmit }) {
-  const [search, setSearch] = React.useState('');
+  const [search, setSearch] = React.useState("");
+  const [emptyWarnClasses, setEmptyWarnClasses] = React.useState("hidden");
 
   return (
-    <CForm onSubmit={(event) => {
-      event.preventDefault();
-      handleSubmit(search);
-    }}>
-      <CFormLabel htmlFor="search-box" className="visually-hidden">Search the dictionary</CFormLabel>
-      <CFormInput
-        id="search-box"
-        placeholder="Search for any word..."
-        className={styles.input}
-        aria-describedby="feedback-text"
-        required
-        value={search}
-        onChange={(event) => {
-          setSearch(event.target.value);
-        }}
-      />
-      <CFormText hidden component="span" id="feedback-text" className={styles.emptyerror}>
+    <CForm
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (!search) {
+          setEmptyWarnClasses("");
+        } else {
+          setEmptyWarnClasses("hidden");
+          handleSubmit(search);
+        }
+      }}
+    >
+      <CFormLabel htmlFor="search-box" className="visually-hidden">
+        Search the dictionary
+      </CFormLabel>
+      <div className={styles.inputwrapper}>
+        <CFormInput
+          id="search-box"
+          placeholder="Search for any word..."
+          className={styles.input}
+          aria-describedby="empty-help"
+          value={search}
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+        >
+          <path
+            fill="none"
+            stroke="#A445ED"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="m12.663 12.663 3.887 3.887M1 7.664a6.665 6.665 0 1 0 13.33 0 6.665 6.665 0 0 0-13.33 0Z"
+          />
+        </svg>
+      </div>
+      <CFormText component="span" id="empty-help" className={emptyWarnClasses}>
         Whoops, can't be empty...
       </CFormText>
-
     </CForm>
   );
 }
