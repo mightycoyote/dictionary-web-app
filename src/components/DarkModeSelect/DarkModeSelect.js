4a79d327cf9761fd6a5778ca5277cfb939e6becc
Sarah Wilkes <aremnius@gmail.com>
2023-05-04 16:13:49 -0400

    more styling

--- src/components/DarkModeSelect/DarkModeSelect.js
+++ src/components/DarkModeSelect/DarkModeSelect.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./DarkModeSelect.module.scss";
-import moon from "../../assets/images/icon-moon.svg";
 import { CFormSwitch } from "@coreui/react";
 
 function DarkModeSelect({ darkmodeOn, setDarkmodeOn }) {
   return (
-    <form className={styles.darkmodeform}>
+    <form 
+      onClick={() => setDarkmodeOn(!darkmodeOn)}
+      className={styles.darkmodeform}
+    >
       <CFormSwitch
-        label={<Moon />}
+        // label="Toggle Darkmode"
         id="formSwitchCheckDefault"
         checked={darkmodeOn}
         onChange={(event) => {
@@ -15,12 +17,29 @@ function DarkModeSelect({ darkmodeOn, setDarkmodeOn }) {
         }}
         className={styles.switch}
       />
+      <div>
+        <svg
+        // put in accessibility stuff
+          id="svg"
+          xmlns="http://www.w3.org/2000/svg"
+          width="22"
+          height="22"
+          viewBox="0 0 22 22"
+          className={styles.svg}
+        >
+          <path
+            className={styles.svg}
+            fill="none"
+            stroke="#838383"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
+          />
+        </svg>
+      </div>
     </form>
   );
-}
-
-function Moon() {
-  return <img src={moon} alt="Toggle Darkmode" />;
 }
 
 export default DarkModeSelect;
