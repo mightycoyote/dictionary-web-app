ed7afae5bbdc8f3791fe6cdee3d413f3cdce49ce
Sarah Wilkes <aremnius@gmail.com>
2023-05-09 16:44:51 -0400

    css cleanup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,14 @@
 import React from "react";
+import Logo from "../../assets/images/logo.svg"
 import FontSelect from "../FontSelect";
 import DarkModeSelect from "../DarkModeSelect";
 
 function Header({ font, setFont, darkmodeOn, setDarkmodeOn }) {
   return (
     <header>
-      <svg
-        title="Dictionary Search logo"
-        xmlns="http://www.w3.org/2000/svg"
-        width="34"
-        height="38"
-        viewBox="0 0 34 38"
-      >
-        <g
-          fill="none"
-          fill-rule="evenodd"
-          stroke="#838383"
-          stroke-linecap="round"
-          stroke-width="1.5"
-        >
-          <path d="M1 33V5a4 4 0 0 1 4-4h26.8A1.2 1.2 0 0 1 33 2.2v26.228M5 29h28M5 37h28" />
-          <path stroke-linejoin="round" d="M5 37a4 4 0 1 1 0-8" />
-          <path d="M11 9h12" />
-        </g>
-      </svg>
+      <div>
+        <img src={Logo} alt="Dictionary Search logo" />
+      </div>
 
       <div className="selectors">
         <FontSelect font={font} setFont={setFont} />
