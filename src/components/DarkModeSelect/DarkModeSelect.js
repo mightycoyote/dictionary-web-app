6d6d845d7240a1af1c08560d73765924351b5faf
Sarah Wilkes <aremnius@gmail.com>
2023-05-12 11:14:50 -0400

    fix inline svgs

--- src/components/DarkModeSelect/DarkModeSelect.js
+++ src/components/DarkModeSelect/DarkModeSelect.js
@@ -5,15 +5,12 @@ function DarkModeSelect({ darkmodeOn, setDarkmodeOn }) {
   return (
     <form onClick={() => setDarkmodeOn(!darkmodeOn)} className="darkmodeform">
       <CFormSwitch
-        // id="formSwitchCheckDefault"
         checked={darkmodeOn}
         onChange={(event) => {
           setDarkmodeOn(event.target.checked);
         }}
       />
-      {/* <div className="moondiv">  */}
       <svg
-        // put in accessibility stuff
         title="Toggle Darkmode"
         xmlns="http://www.w3.org/2000/svg"
         width="22"
@@ -23,13 +20,12 @@ function DarkModeSelect({ darkmodeOn, setDarkmodeOn }) {
         <path
           fill="none"
           stroke="#838383"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
           d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
         />
       </svg>
-      {/* </div> */}
     </form>
   );
 }
