3c9cd57a5f0bc201a0e560d32e8d871b0a862650
Sarah Wilkes <aremnius@gmail.com>
2023-08-08 15:01:35 -0400

    fix no result component display

--- src/components/NoResult/NoResult.js
+++ src/components/NoResult/NoResult.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function NoResult({result}) {
   return (
-    <React.Fragment className="noresult">
+    <div className="noresult">
       <p className="emoji" role="img" aria-label="confused face">😕</p>
       <h1>{result.title}</h1>
       <p>{result.message} {result.resolution}</p>
-    </React.Fragment>
+    </div>
   );
 }
 
